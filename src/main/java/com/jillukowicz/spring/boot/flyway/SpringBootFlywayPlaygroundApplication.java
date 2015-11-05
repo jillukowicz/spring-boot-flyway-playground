876c8e9929b37af4cb6ea32720d199b50e52c844
jillukowicz <jakub.illukowicz@gmail.com>
2015-11-05 09:42:27 +0100

    init

---
+++ src/main/java/com/jillukowicz/spring/boot/flyway/SpringBootFlywayPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.jillukowicz.spring.boot.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlywayPlaygroundApplication.class, args);
+    }
+}
